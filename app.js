f4ba3fb4245f7d588d5ad2775969196ebb13f68d
Mr-vabs <vk8400033581@gmail.com>
2022-10-11 21:48:59 +0530

    creation of list and styling

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
 
-// Variables 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-let items = ["Node rocks!", "React is awful.", "MERN is greedy."];
+const items = ["Node rocks!", "React is awful.", "MERN is greedy."];
 
 app.get("/", function(req, res){
-    let today = new Date();
-    // let resmesg = (currDay === 6 || currDay === 0) ? "Weekend, Let's watch movies and chill bro!" : "Weekday, Get back to office man!";
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    let currDay = today.toLocaleDateString("hi-IN", options);
+    let currDay = date.getDate();
     res.render('list', {day: currDay, newListItems: items});
 });
 
@@ -27,7 +19,6 @@ app.post("/", function(req, res){
     let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
-    // console.log(formInput);
 });
 
 app.listen(3000, function(){
